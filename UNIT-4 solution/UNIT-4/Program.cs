450e17397604e4e6d7ea11bcf502299775c5f435
gauravkumarsanghani126725-ops <gauravkumar.sanghani126725@marwadiuniversity.ac.in>
2026-02-24 08:44:44 +0530

    Add files via upload

---
+++ UNIT-4 solution/UNIT-4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNIT_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            rectangle r = new rectangle();
+            r.getdata();
+            r.calculate();
+            r.display();
+
+            circle c = new circle();
+            c.getdata();
+            c.cal();
+            c.display();
+        }
+    }
+}
