0c3d76597b22e1ecbed0025a697e00142c62bc8f
gauravkumarsanghani126725-ops <gauravkumar.sanghani126725@marwadiuniversity.ac.in>
2026-03-20 12:58:17 +0530

    Add files via upload

---
+++ UNIT-4 solution/emp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            detalis d = new detalis();
+            d.emp_detalis();
+            d.display();
+        }
+    }
+}
