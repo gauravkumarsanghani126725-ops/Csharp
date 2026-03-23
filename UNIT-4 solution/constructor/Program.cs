92616daa80944d6cb4b3116b70a6a6bc4d2a96be
gauravkumarsanghani126725-ops <gauravkumar.sanghani126725@marwadiuniversity.ac.in>
2026-03-23 10:42:19 +0530

    Add files via upload

---
+++ UNIT-4 solution/constructor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle();
+            r1.calculate();
+            r1.Display();
+
+            Rectangle r2 = new Rectangle(30,40);
+            r2.calculate();
+            r2.Display();
+        }
+    }
+}
